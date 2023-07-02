c6a51fe57b3089469cf50cad9f2bf5cb0330dfa0
Joel Elrod <joelelrod246@gmail.com>
2023-07-02 10:56:28 -0400

    Added boilerplate stuff
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

--- lc_3Sum/Program.cs
+++ lc_3Sum/Program.cs
@@ -6,6 +6,13 @@ public class Solution
 {
     static public IList<IList<int>> ThreeSum(int[] nums)
     {
+        IList<IList<int>> triplets = new List<IList<int>>();
 
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+        }
+
+        return triplets;
     }
 }
